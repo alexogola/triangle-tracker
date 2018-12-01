bfb6dd34d353dc101f6bbaae0972533bb350a2f9
Alex Ogola <ogollaokumu@gmail.com>
2018-12-01 10:04:05 +0300

    adjust alert messages

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,15 @@ function myTriangle(){
 
 		triangle.push(parseFloat(document.getElementById("sideA").value));
 			if(isNaN(triangle[0]) === true || triangle[0] <= 0){
-				alert("Please enter a valid dimension");
+				alert("Please enter valid dimensions");
 			}else{
 				triangle.push(parseFloat(document.getElementById("sideB").value));
 					if(isNaN(triangle[1]) === true || triangle[1] <= 0){
-						alert("Please enter a valid dimension");
+						alert("Please enter valid dimensions");
 					}else{
 						triangle.push(parseFloat(document.getElementById("sideC").value));
 						if(isNaN(triangle[2]) === true || triangle[2] <= 0){
-							alert("Please enter a valid dimension");
+							alert("Please enter valid dimensions");
 						}else{
 							if((triangle[0] + triangle[1]) <= triangle[2] || (triangle[1] + triangle[2]) <= triangle[0] || (triangle[0] + triangle[2]) <= triangle[1]){
 								alert("These measurements cannot form a triangle.");
